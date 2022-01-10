c6a5df6798506df69001386fd750333613a3684e
beansBin <beans_bin@naver.com>
2022-01-10 20:49:06 +0900

    [ADD] MainCoordinator 파일 및 코드 생성

---
+++ CoordinatorPractice/CoordinatorPractice/View/MainCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  MainCoordinator.swift
+//  CoordinatorPractice
+//
+//  Created by 박예빈 on 2022/01/10.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coodinator {
+    var childCoordinator: [Coodinator] = []
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let mainViewController = MainViewController()
+        MainViewController.coordinator = self
+        navigationController.pushViewController(mainViewController, animated: true)
+    }
+}
