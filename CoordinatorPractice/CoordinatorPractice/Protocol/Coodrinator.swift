202aed64297a89f809152eb3750e84d9521af87a
beansBin <beans_bin@naver.com>
2022-01-10 20:46:15 +0900

    [ADD] Coodinator 파일 및 코드 생성

---
+++ CoordinatorPractice/CoordinatorPractice/Protocol/Coodrinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coodrinator.swift
+//  CoordinatorPractice
+//
+//  Created by 박예빈 on 2022/01/10.
+//
+
+import UIKit
+
+protocol Coodinator: class {
+    var childCoordinator: [Coodinator] { get set } // 자식 코디네이터를 저장하는 변수
+    var navigationController: UINavigationController { get set } // 네비게이션 스택을 쌓을 NVC
+    
+    func start() // 화면 전환 로직 역할 수행
+}
