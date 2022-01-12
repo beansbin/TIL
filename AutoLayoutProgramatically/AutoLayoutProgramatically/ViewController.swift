526eb40e34f5f7c9d74f1d4cb8de8a5e8bae1d56
beansBin <beans_bin@naver.com>
2022-01-12 14:07:39 +0900

    [DEL] Storyboard 삭제

---
+++ AutoLayoutProgramatically/AutoLayoutProgramatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgramatically
+//
+//  Created by 박예빈 on 2022/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
