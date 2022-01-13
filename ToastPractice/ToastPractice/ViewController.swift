4763388173388dc7d8c89b99b88ea507a533e7c1
beansBin <beans_bin@naver.com>
2022-01-14 00:14:03 +0900

    [FEAT] 토스트 메시지 띄우기

--- ToastPractice/ToastPractice/ViewController.swift
+++ ToastPractice/ToastPractice/ViewController.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("토스트 띄우기", for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false // 코드로 오토레이아웃을 사용하기 위해 설정
+        button.centerXAnchor.constraint(equalTo:view.centerXAnchor)
+            .isActive = true // x축을 기준으로 가운데 정렬
+        button.centerYAnchor.constraint(equalTo:view.centerYAnchor)
+            .isActive = true // y축을 기준으로 가운데 정렬
+        button.heightAnchor.constraint(equalToConstant: 200)
+            .isActive = true // 너비를 200으로 고정
+        button.widthAnchor.constraint(equalToConstant: 200)
+            .isActive = true // 높이를 200으로 고정
+        button.addTarget(self, action: #selector(showToast(_:)), for: .touchUpInside)
         
     }
     
-    func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) { let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+    @objc func showToast(_ sender: UIButton) {
+        let message: String = "토스트 메시지"
+        let font: UIFont = UIFont.systemFont(ofSize: 14.0)
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75,
+                                               y: self.view.frame.size.height-100,
+                                               width: 150,
+                                               height: 35))
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         toastLabel.textColor = UIColor.white
         toastLabel.font = font
@@ -34,5 +58,6 @@ class ViewController: UIViewController {
         })
         
     }
+
 }
 
