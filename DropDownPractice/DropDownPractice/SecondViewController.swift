b52f0884dcafbcb085b0f14de86e67581e7b6c85
beansBin <beans_bin@naver.com>
2022-01-13 18:14:59 +0900

    [ADD] SecondViewController 파일 생성

---
+++ DropDownPractice/DropDownPractice/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  DropDownPractice
+//
+//  Created by 박예빈 on 2022/01/13.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+    }
+    
+
+}
--- DropDownPractice/DropDownPractice/ViewController.swift
+++ DropDownPractice/DropDownPractice/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        navigationController?.title = "Gg"
+        print("나오긴 하니?")
     }
 
 
