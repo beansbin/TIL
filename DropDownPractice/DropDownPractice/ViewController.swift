acc66618ca3878fc594991085547d92c0f1e35d4
beansBin <beans_bin@naver.com>
2022-01-13 18:26:09 +0900

    [ADD] 버튼 생성 및 toGoNext() 연결

--- DropDownPractice/DropDownPractice/ViewController.swift
+++ DropDownPractice/DropDownPractice/ViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var button : UIButton = {
+        let btn = UIButton()
+        btn.setTitle("다음 뷰로", for: .normal)
+        btn.backgroundColor = .red
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         navigationController?.title = "Gg"
         print("나오긴 하니?")
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(toGoNext(_:)), for: .touchUpInside)
+        
+        setAutoLayouts()
     }
-
-
+    
+    func setAutoLayouts() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo:view.centerXAnchor)
+            .isActive = true // x축을 기준으로 가운데 정렬
+        button.centerYAnchor.constraint(equalTo:view.centerYAnchor)
+            .isActive = true // y축을 기준으로 가운데 정렬
+        button.heightAnchor.constraint(equalToConstant: 200)
+            .isActive = true // 너비를 200으로 고정
+        button.widthAnchor.constraint(equalToConstant: 200)
+            .isActive = true // 높이를 200으로 고정
+        
+    }
+    
+    @objc func toGoNext(_ sender: UIButton) {
+        print("부들부들")
+        self.navigationController?.pushViewController(SecondViewController(), animated: true)
+    }
+    
+    
+    
+    
 }
 
