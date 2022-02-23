5414c9e6c41214fccd7576df12040969ddde2ef2
beansBin <beans_bin@naver.com>
2022-02-23 15:40:01 +0900

    [FEAT] UI 구성

---
+++ PageMenuPractice/PageMenuPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageMenuPractice
+//
+//  Created by 박예빈 on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
